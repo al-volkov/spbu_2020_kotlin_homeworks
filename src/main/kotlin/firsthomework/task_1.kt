27ecf75fee75151986889ba4edccc5cc28578dca
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2021-02-20 22:37:46 +0300

    Update task_1.kt

--- src/main/kotlin/firsthomework/task_1.kt
+++ src/main/kotlin/firsthomework/task_1.kt
@@ -1,25 +1,21 @@
 package firsthomework
 
-object IterativeFactorial {
-    fun get(n: Int): Int {
-        var value = 1
-        for (i in 2..n)
-            value *= i
-        return value
-    }
+fun getIterativeFactorial(n: Int): Int {
+    var value = 1
+    for (i in 2..n)
+        value *= i
+    return value
 }
 
-object RecursiveFactorial {
-    fun get(n: Int): Int {
-        if (n == 0) return 1
-        return n * get(n - 1)
-    }
+fun getRecursiveFactorial(n: Int): Int {
+    if (n == 0) return 1
+    return n * getRecursiveFactorial(n - 1)
 }
 
 fun main() {
     println("Enter a non-negative integer")
     val number: Int = readLine()?.toInt() ?: 0
-    println("Factorial of $number, calculated iteratively, equals ${IterativeFactorial.get(number)}")
-    println("Factorial of $number, calculated recursively, equals ${RecursiveFactorial.get(number)}")
+    println("Factorial of $number, calculated iteratively, equals ${getIterativeFactorial(number)}")
+    println("Factorial of $number, calculated recursively, equals ${getRecursiveFactorial(number)}")
     return
 }
