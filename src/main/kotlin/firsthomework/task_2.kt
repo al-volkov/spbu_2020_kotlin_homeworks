518baa34108e461f6a4f8aad76ec419f8da0fb39
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2021-02-24 19:05:57 +0300

    Update task_2.kt

--- src/main/kotlin/firsthomework/task_2.kt
+++ src/main/kotlin/firsthomework/task_2.kt
@@ -1,26 +1,26 @@
 package firsthomework
 
-val scanner = java.util.Scanner(System.`in`)
-
-fun numberOfOccurrences(string1: String, string2: String): Int {
+fun String.numberOfOccurrences(substr: String): Int {
+    if ((this.length < substr.length) || (kotlin.math.min(this.length, substr.length) == 0)) {
+        return 0
+    }
     var count = 0
-    for (i in 0..(string1.length - string2.length)) {
-        if (string1[i] == string2[0]) {
-            if (string1.substring(i, i + string2.length) == string2) {
-                ++count
-            }
+    for (i in this.windowed(substr.length)) {
+        if (i == substr) {
+            ++count
         }
     }
     return count
 }
 
 fun main() {
+    val scanner = java.util.Scanner(System.`in`)
     println("Enter first string")
     val string1 = scanner.nextLine()
     println("Enter second string")
     val string2 = scanner.nextLine()
-    println("The second line is " +
-            "${if (string1.length >= string2.length) numberOfOccurrences(string1,string2) else 0}" +
-            " times included in the first as a substring")
-    return
+    val output = "The second line is " +
+            "${string1.numberOfOccurrences(string2)}" +
+            " times included in the first as a substring"
+    println(output)
 }
