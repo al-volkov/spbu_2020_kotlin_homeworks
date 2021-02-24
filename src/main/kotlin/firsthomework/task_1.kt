226ecd4422ddf4348c8d291cb3830f68f5ca639a
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2021-02-24 18:23:52 +0300

    fixed problems with incorrect input processing

--- src/main/kotlin/firsthomework/task_1.kt
+++ src/main/kotlin/firsthomework/task_1.kt
@@ -2,20 +2,33 @@ package firsthomework
 
 fun getIterativeFactorial(n: Int): Int {
     var value = 1
-    for (i in 2..n)
+    for (i in 2..n) {
         value *= i
+    }
     return value
 }
 
 fun getRecursiveFactorial(n: Int): Int {
-    if (n == 0) return 1
+    if (n == 0) {
+        return 1
+    }
     return n * getRecursiveFactorial(n - 1)
 }
 
 fun main() {
     println("Enter a non-negative integer")
-    val number: Int = readLine()?.toInt() ?: 0
+    var number: Int
+    val input: String = java.util.Scanner(System.`in`).next()
+    try {
+        number = input.toInt()
+    } catch (e: NumberFormatException) {
+        println("Error! The entered string cannot be interpreted as an integer")
+        return
+    }
+    if (number < 0) {
+        println("Error! A negative number has been entered.")
+        return
+    }
     println("Factorial of $number, calculated iteratively, equals ${getIterativeFactorial(number)}")
     println("Factorial of $number, calculated recursively, equals ${getRecursiveFactorial(number)}")
-    return
 }
