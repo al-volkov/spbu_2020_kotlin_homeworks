e9ba394406a1fbe5891756f84f22ba465a2e6e97
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2021-03-05 18:06:31 +0300

    style fix

--- src/main/kotlin/homework_2/task_2.kt
+++ src/main/kotlin/homework_2/task_2.kt
@@ -1,13 +1,17 @@
 package homework_2
 
-fun Array<Int>.removeDuplicateElements(): Array<Int> {
+const val NUMBER1 = 1
+const val NUMBER2 = 2
+const val NUMBER3 = 3
+
+fun IntArray.removeDuplicateElements(): IntArray {
     val set: LinkedHashSet<Int> = linkedSetOf()
-    set.addAll(this)
-    return set.toTypedArray()
+    set.addAll(this.toTypedArray())
+    return set.toIntArray()
 }
 
 fun main() {
-    val testArray = arrayOf(1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 6, 7)
+    val testArray = intArrayOf(NUMBER1, NUMBER2, NUMBER3, NUMBER3, NUMBER1)
     print(testArray.joinToString())
     print("\n")
     print(testArray.removeDuplicateElements().joinToString())
