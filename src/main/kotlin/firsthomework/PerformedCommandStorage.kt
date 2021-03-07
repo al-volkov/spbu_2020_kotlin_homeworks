666f66841ced4b117044414034f650bfee7d20c8
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2021-03-07 19:42:38 +0300

    added documentation

--- src/main/kotlin/firsthomework/PerformedCommandStorage.kt
+++ src/main/kotlin/firsthomework/PerformedCommandStorage.kt
@@ -2,6 +2,12 @@ package firsthomework
 
 import kotlin.collections.ArrayDeque
 
+/**
+ * Stores a list of numbers and actions on it
+ * @property arrayDeque Public property for accessing [_arrayDeque]
+ * @property _arrayDeque stores numbers
+ * @property actions keeps performed actions
+ */
 class PerformedCommandStorage {
     val arrayDeque: ArrayDeque<Int>
         get() {
@@ -9,7 +15,15 @@ class PerformedCommandStorage {
         }
     private val _arrayDeque: ArrayDeque<Int> = ArrayDeque()
     private val actions: java.util.Stack<Action> = java.util.Stack()
+
+    /**
+     * adds and [action] to [actions]
+     */
     fun addAction(action: Action) = actions.addElement(action)
+
+    /**
+     * calls undo() on the last element if [actions] and removes it
+     */
     fun undo() {
         if (actions.isEmpty()) {
             println("No action has been taken yet.")
@@ -19,6 +33,9 @@ class PerformedCommandStorage {
         }
     }
 
+    /**
+     * prints all the elements in the list
+     */
     fun print() {
         if (this._arrayDeque.size == 0) {
             println("No elements (empty queue)")
