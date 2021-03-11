865851b39604cc84ca9db7f2798e40b4d9c3102c
Alexander-Volkov-144 <70965603+Alexander-Volkov-144@users.noreply.github.com>
2021-03-11 03:05:31 +0300

    working version

--- src/main/kotlin/firsthomework/Action.kt
+++ src/main/kotlin/firsthomework/Action.kt
@@ -1,6 +1,7 @@
 package firsthomework
 
 import java.lang.IndexOutOfBoundsException
+import kotlinx.serialization.Serializable
 
 private fun ArrayDeque<Int>.moveElement(startingIndex: Int, finalIndex: Int) {
     if (kotlin.math.max(startingIndex, finalIndex) >= this.size || kotlin.math.min(startingIndex, finalIndex) < 0) {
@@ -11,43 +12,44 @@ private fun ArrayDeque<Int>.moveElement(startingIndex: Int, finalIndex: Int) {
     this.add(finalIndex, valueOfElement)
 }
 
-interface Action {
-    fun undo()
+@Serializable
+sealed class Action {
+    abstract fun execute(storage: PerformedCommandStorage)
+    abstract fun undo(storage: PerformedCommandStorage)
 }
 
-class PushForward(value: Int, private val storage: PerformedCommandStorage) : Action {
-    init {
+@Serializable
+class PushForward(private val value: Int) : Action() {
+    override fun execute(storage: PerformedCommandStorage) {
         storage.arrayDeque.addFirst(value)
         storage.addAction(this)
     }
 
-    override fun undo() {
+    override fun undo(storage: PerformedCommandStorage) {
         storage.arrayDeque.removeFirst()
     }
 }
 
-class PushBack(value: Int, private val storage: PerformedCommandStorage) : Action {
-    init {
+@Serializable
+class PushBack(private val value: Int) : Action() {
+    override fun execute(storage: PerformedCommandStorage) {
         storage.arrayDeque.addLast(value)
         storage.addAction(this)
     }
 
-    override fun undo() {
+    override fun undo(storage: PerformedCommandStorage) {
         storage.arrayDeque.removeLast()
     }
 }
 
-class MoveElement(
-    private val startingIndex: Int,
-    private val finalIndex: Int,
-    private val storage: PerformedCommandStorage
-) : Action {
-    init {
+@Serializable
+class MoveElement(private val startingIndex: Int, private val finalIndex: Int) : Action() {
+    override fun execute(storage: PerformedCommandStorage) {
         storage.arrayDeque.moveElement(startingIndex, finalIndex)
         storage.addAction(this)
     }
 
-    override fun undo() {
+    override fun undo(storage: PerformedCommandStorage) {
         storage.arrayDeque.moveElement(finalIndex, startingIndex)
     }
 }
--- src/main/kotlin/firsthomework/PerformedCommandStorage.kt
+++ src/main/kotlin/firsthomework/PerformedCommandStorage.kt
@@ -1,6 +1,11 @@
 package firsthomework
 
+import kotlinx.serialization.decodeFromString
 import kotlin.collections.ArrayDeque
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+import java.io.FileWriter
 
 class PerformedCommandStorage {
     val arrayDeque: ArrayDeque<Int>
@@ -14,7 +19,7 @@ class PerformedCommandStorage {
         if (actions.isEmpty()) {
             println("No action has been taken yet.")
         } else {
-            actions.lastElement().undo()
+            actions.lastElement().undo(this)
             actions.removeLast()
         }
     }
@@ -24,6 +29,22 @@ class PerformedCommandStorage {
             println("No elements (empty queue)")
         } else {
             println(this._arrayDeque.joinToString(" "))
+        }
+    }
+
+    fun serialize(path: String) {
+        val temporaryList = actions.toList()
+        val stringInJsonFormat = Json.encodeToString(temporaryList)
+        val newFile = FileWriter(path)
+        newFile.write(stringInJsonFormat)
+        newFile.flush()
+    }
+
+    fun deserialize(path: String) {
+        val stringInJsonFormat = File(path).inputStream().readAllBytes().toString(Charsets.UTF_8)
+        val listWithActions = Json.decodeFromString<List<Action>>(stringInJsonFormat)
+        for (action in listWithActions) {
+            action.execute(this)
         }
     }
 }
