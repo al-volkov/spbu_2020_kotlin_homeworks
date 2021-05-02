709e0b78974e5bede2a8de58a6e3198815949c21
Alexander-Volkov-144 <70965603+volkov-alexander-d@users.noreply.github.com>
2021-05-02 18:48:55 +0300

    Update TicTacToe.kt

--- src/main/kotlin/homework_8/TicTacToe.kt
+++ src/main/kotlin/homework_8/TicTacToe.kt
@@ -4,7 +4,7 @@ import homework_8.views.StartView
 import javafx.stage.Stage
 import tornadofx.App
 
-class TicTacToe : App(StartView::class, GameStylesheet::class){
+class TicTacToe : App(StartView::class, GameStylesheet::class) {
     override fun start(stage: Stage) {
         stage.isResizable = false
         super.start(stage)
